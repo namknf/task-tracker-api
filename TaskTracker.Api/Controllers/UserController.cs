93eede6eb4222c646ca47c75fd48223570403aed
namknf <9093264418es@gmail.com>
2023-10-02 20:51:43 +0300

    registration/authorization methods

--- TaskTracker.Api/Controllers/UserController.cs
+++ TaskTracker.Api/Controllers/UserController.cs
@@ -1,10 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Api.ActionFilters;
+using TaskTracker.Contract;
+using TaskTracker.Entities.DataTransferObjects;
+using TaskTracker.Entities.Models;
 
 namespace TaskTracker.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IAuthenticationService _authService;
+
+        public UserController(ILogger<UserController> logger, IMapper mapper, UserManager<User> userManager, IAuthenticationService authService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _authService = authService;
+        }
+
+        /// <summary>
+        /// Регистрация нового пользователя
+        /// </summary>
+        /// <param name="userForRegistration">Модель пользователя для регистрации</param>
+        /// <returns>Зарегистрированный пользователь</returns>
+        [HttpPost("register")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrerDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Авторизация
+        /// </summary>
+        /// <param name="user">Авторизованный пользователь</param>
+        /// <returns>Токен</returns>
+        [HttpPost("login")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Authenticate([FromBody] UserForAuthorizeDto user)
+        {
+            if (!await _authService.ValidateUser(user))
+            {
+                _logger.LogWarning($"{nameof(Authenticate)}: Authentication failed. Wrong user name or password.");
+                return Unauthorized();
+            }
+            return Ok(new { Token = await _authService.CreateToken() });
+        }
     }
 }
