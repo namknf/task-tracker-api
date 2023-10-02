4caae24459da1153ed62e84bc6ee7d92667eded0
namknf <9093264418es@gmail.com>
2023-10-02 20:52:10 +0300

    add mapping

---
+++ TaskTracker.Api/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskTracker.Entities.DataTransferObjects;
+using TaskTracker.Entities.Models;
+
+namespace TaskTracker.Api
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserForRegistrerDto, User>();
+        }
+    }
+}
