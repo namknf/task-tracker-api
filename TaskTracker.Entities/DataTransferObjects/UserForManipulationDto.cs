df656edabb99f45f9f7ca6f550fcd771876caaa6
namknf <9093264418es@gmail.com>
2023-10-12 18:20:37 +0300

    dto task, add requirements for user dtos

--- TaskTracker.Entities/DataTransferObjects/UserForManipulationDto.cs
+++ TaskTracker.Entities/DataTransferObjects/UserForManipulationDto.cs
@@ -4,9 +4,10 @@ namespace TaskTracker.Entities.DataTransferObjects
 {
     public abstract class UserForManipulationDto
     {
-        [Required(ErrorMessage = "Password name is required")]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
     }
 }
--- TaskTracker.Entities/DataTransferObjects/UserForRegistrerDto.cs
+++ TaskTracker.Entities/DataTransferObjects/UserForRegistrerDto.cs
@@ -1,11 +1,16 @@
-﻿namespace TaskTracker.Entities.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Entities.DataTransferObjects
 {
     public class UserForRegistrerDto : UserForManipulationDto
     {
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Position is required")]
         public string Position { get; set; }
     }
 }
