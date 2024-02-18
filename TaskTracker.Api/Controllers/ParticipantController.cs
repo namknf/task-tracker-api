b30b64569973c542805a243d5312674c971fe9bb
namknf <9093264418es@gmail.com>
2024-02-18 11:48:17 +0300

    get participants method

---
+++ TaskTracker.Api/Controllers/ParticipantController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Entities.DataTransferObjects;
+using TaskTracker.Entities.Models;
+
+namespace TaskTracker.Api.Controllers
+{
+    [Route("api/participants")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ParticipantController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public ParticipantController(IMapper mapper, UserManager<User> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Get participants to create project or task
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Authorize]
+        public ActionResult<List<ParticipantForGetDto>> GetParticipants()
+        {
+            var participantsFromDb = _userManager.Users;
+            var participants = _mapper.Map<List<ParticipantForGetDto>>(participantsFromDb);
+            return Ok(participants);
+        }
+    }
+}
