fe6b1bf275ee4f8c1c92cd14984d594d7e03cc8f
namknf <9093264418es@gmail.com>
2024-05-19 13:52:22 +0300

    add migration password reset confirmation code

---
+++ TaskTracker.Api/Migrations/20240519104740_AddPasswordCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Api.Migrations
+{
+    public partial class AddPasswordCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordCode",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordCode",
+                table: "AspNetUsers");
+        }
+    }
+}
